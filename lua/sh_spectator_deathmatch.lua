97017c25b58e54b7cbaab87fce1f7167eac42191
Ouazzany Ismaïl <ouazzany.i@gmail.com>
2015-01-28 12:33:37 +0000

    Fix spawn problem

--- lua/sh_spectator_deathmatch.lua
+++ lua/sh_spectator_deathmatch.lua
@@ -28,11 +28,12 @@ hook.Add("OnEntityCreated", "OnEntityCreated_Ghost", function(ent)
 end)
 
 hook.Add("ShouldCollide", "ShouldCollide_Ghost", function(ent1, ent2)
-	if not IsValid(ent1) or not IsValid(ent2) then return false end
-	if ent1:IsPlayer() and (ent1.IsGhost and ent1:IsGhost()) and not (ent2:IsPlayer() and (ent2.IsGhost and ent2:IsGhost())) then
-		return false
-	end
-	if ent2:IsPlayer() and (ent2.IsGhost and ent2:IsGhost()) and not (ent1:IsPlayer() and (ent1.IsGhost and ent1:IsGhost())) then
-		return false
+	if IsValid(ent1) and IsValid(ent2) then
+		if ent1:IsPlayer() and (ent1.IsGhost and ent1:IsGhost()) and not (ent2:IsPlayer() and (ent2.IsGhost and ent2:IsGhost())) then
+			return false
+		end
+		if ent2:IsPlayer() and (ent2.IsGhost and ent2:IsGhost()) and not (ent1:IsPlayer() and (ent1.IsGhost and ent1:IsGhost())) then
+			return false
+		end
 	end
 end)
