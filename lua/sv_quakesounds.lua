7eec6357b2f81eb2037ef2d28c937c273bc7144d
Min <MinIsMin@users.noreply.github.com>
2017-01-06 13:46:08 +0100

    Reverted change "SpecDM_QuakeSound ply fix"
    Wasn't working, thanks @markusmarkusz

--- lua/sv_quakesounds.lua
+++ lua/sv_quakesounds.lua
@@ -2,6 +2,7 @@
 local function SpecDM_SendQuake(ply)
 	if not tonumber(ply.specdm_killrows) or not tonumber(ply.specdm_close_kills) then return end
 	net.Start("SpecDM_QuakeSound")
+	net.WriteEntity(ply)
 	net.WriteUInt(ply.specdm_killrows, 19)
 	net.WriteUInt(ply.specdm_close_kills, 19)
 	local tbl = {}
