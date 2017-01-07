3618ef1f1287d384c3ebc3b17172b4b7f0a6e28e
Markus <markus28988@gmail.com>
2017-01-07 20:31:02 +0100

    Updated GhostHUD (#44)
    * Updated GhostHUD
    
    * Update cl_specdm_hud.lua

--- lua/cl_specdm_hud.lua
+++ lua/cl_specdm_hud.lua
@@ -1,13 +1,13 @@
 
 local dr = draw
 local function ShadowedText(text, font, x, y, color, xalign, yalign)
-	dr.SimpleText(text, font, x+2, y+2, COLOR_BLACK, xalign, yalign)
+	dr.SimpleText(text, font, x + 2, y + 2, COLOR_BLACK, xalign, yalign)
 	dr.SimpleText(text, font, x, y, color, xalign, yalign)
 end
 
 local bg_colors = {
 	background_main = Color(0, 0, 10, 200),
-	noround = Color(100,100,100,200),
+	noround = Color(100, 100, 100, 200),
 	traitor = Color(200, 25, 25, 200),
 	innocent = Color(25, 200, 25, 200),
 	detective = Color(25, 25, 200, 200)
@@ -33,40 +33,42 @@ local function DrawBg(x, y, width, height, client)
 	height = height + th
 	draw.RoundedBox(8, x, y, width, height, bg_colors.background_main)
 	local col = bg_colors.innocent
-	if LocalPlayer():IsGhost() then
-	elseif GAMEMODE.round_state != ROUND_ACTIVE then
+	if client:IsGhost() then
+	elseif GetRoundState() != ROUND_ACTIVE then
 		col = bg_colors.noround
-	elseif LocalPlayer():GetTraitor() then
+	elseif client:GetTraitor() then
 		col = bg_colors.traitor
-	elseif LocalPlayer():GetDetective() then
+	elseif client:GetDetective() then
 		col = bg_colors.detective
 	end
 	draw.RoundedBox(8, x, y, tw, th, col)
 end
 
 local Tex_Corner8 = surface.GetTextureID("gui/corner8")
-local function RoundedMeter( bs, x, y, w, h, color)
+local function RoundedMeter(bs, x, y, w, h, color)
 	surface.SetDrawColor(clr(color))
-	surface.DrawRect( x+bs, y, w-bs*2, h )
-	surface.DrawRect( x, y+bs, bs, h-bs*2 )
-	surface.SetTexture( Tex_Corner8 )
-	surface.DrawTexturedRectRotated( x + bs/2 , y + bs/2, bs, bs, 0 )
-	surface.DrawTexturedRectRotated( x + bs/2 , y + h -bs/2, bs, bs, 90 )
+	surface.DrawRect(x+bs, y, w-bs*2, h)
+	surface.DrawRect(x, y+bs, bs, h-bs*2)
+	surface.SetTexture(Tex_Corner8)
+	surface.DrawTexturedRectRotated(x + bs/2 , y + bs/2, bs, bs, 0)
+	surface.DrawTexturedRectRotated(x + bs/2 , y + h -bs/2, bs, bs, 90)
 	if w > 14 then
-		surface.DrawRect( x+w-bs, y+bs, bs, h-bs*2 )
-		surface.DrawTexturedRectRotated( x + w - bs/2 , y + bs/2, bs, bs, 270 )
-		surface.DrawTexturedRectRotated( x + w - bs/2 , y + h - bs/2, bs, bs, 180 )
+		surface.DrawRect(x+w-bs, y+bs, bs, h-bs*2)
+		surface.DrawTexturedRectRotated(x + w - bs/2 , y + bs/2, bs, bs, 270)
+		surface.DrawTexturedRectRotated(x + w - bs/2 , y + h - bs/2, bs, bs, 180)
 	else
-		surface.DrawRect( x + math.max(w-bs, bs), y, bs/2, h )
+		surface.DrawRect(x + math.max(w-bs, bs), y, bs/2, h)
 	end
 end
 
 local function GetAmmo(ply)
 	local weap = ply:GetActiveWeapon()
 	if not weap or not ply:Alive() then return -1 end
+
 	local ammo_inv = weap:Ammo1() or 0
 	local ammo_clip = weap:Clip1() or 0
 	local ammo_max = weap.Primary.ClipSize or 0
+
 	return ammo_clip, ammo_max, ammo_inv
 end
 
@@ -78,77 +80,67 @@ local function PaintBar(x, y, w, h, colors, value)
 	end
 end
 
-hook.Add("Initialize", "Initialize_GhostHUD", function()
+hook.Add("HUDPaint", "GhostHUD", function()
+	local ply = LocalPlayer()
+	if ply:IsPlayer() and ply:IsGhost() then
+		local ttt_health_label = GetConVar("ttt_health_label")
+		local margin = 10
 
-	local GetLang = LANG.GetUnsafeLanguageTable
+		local L = LANG.GetUnsafeLanguageTable()
+		local width = 250
+		local height = 90
+		local x = margin
+		local y = ScrH() - margin - height
 
-	local ttt_health_label = GetConVar("ttt_health_label")
+		DrawBg(x, y, width, height, ply)
 
-	local margin = 10
-	local old_DrawHUD = GAMEMODE.HUDPaint
-	function GAMEMODE:HUDPaint()
-		if LocalPlayer():IsGhost() then
-			self:HUDDrawTargetID()
-			MSTACK:Draw(LocalPlayer())		
-			TBHUD:Draw(LocalPlayer())
-			WSWITCH:Draw(LocalPlayer())
-			self:HUDDrawPickupHistory()
-			local L = GetLang()
-			local width = 250
-			local height = 90
-			local x = margin
-			local y = ScrH() - margin - height
-			DrawBg(x, y, width, height, LocalPlayer())
-			local bar_height = 25
-			local bar_width = width - (margin*2)
-			local health = math.max(0, LocalPlayer():Health())
-			local health_y = y + margin
-			PaintBar(x + margin, health_y, bar_width, bar_height, health_colors, health/100)
-			ShadowedText(tostring(health), "HealthAmmo", bar_width, health_y, COLOR_WHITE, TEXT_ALIGN_RIGHT, TEXT_ALIGN_RIGHT)
-			if ttt_health_label:GetBool() then
-				local health_status = util.HealthToString(health)
-				draw.SimpleText(L[health_status], "TabLarge", x + margin*2, health_y + bar_height/2, COLOR_WHITE, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-			end
-			if LocalPlayer():GetActiveWeapon().Primary then
-				local ammo_clip, ammo_max, ammo_inv = GetAmmo(LocalPlayer())
-				if ammo_clip != -1 then
-					local ammo_y = health_y + bar_height + margin
-					PaintBar(x+margin, ammo_y, bar_width, bar_height, ammo_colors, ammo_clip/ammo_max)
-					local text = string.format("%i + %02i", ammo_clip, ammo_inv)
-					ShadowedText(text, "HealthAmmo", bar_width, ammo_y, COLOR_WHITE, TEXT_ALIGN_RIGHT, TEXT_ALIGN_RIGHT)
-				end
-			end
-			local text = "Ghost DM"
-			local traitor_y = y - 30
-			ShadowedText(text, "TraitorState", x + margin + 73, traitor_y, COLOR_WHITE, TEXT_ALIGN_CENTER)
-			local is_haste = HasteMode() and round_state == ROUND_ACTIVE
-			local endtime = GetGlobalFloat("ttt_round_end", 0) - CurTime()
-			local text
-			local font = "TimeLeft"
-			local color = COLOR_WHITE
-			local rx = x + margin + 170
-			local ry = traitor_y + 3
-			if is_haste then
-				local hastetime = GetGlobalFloat("ttt_haste_end", 0) - CurTime()
-				if hastetime < 0 then
-					text = L.overtime
-					font = "Trebuchet18"
-					ry = ry + 5
-					rx = rx - 3
-				else
-					local t = hastetime
-					text = util.SimpleTime(math.max(0, t), "%02i:%02i")
-				end
-			else
-				text = util.SimpleTime(math.max(0, endtime), "%02i:%02i")
-			end
-			ShadowedText(text, font, rx, ry, color)
-			if is_haste then
-				dr.SimpleText(L.hastemode, "TabLarge", x + margin + 165, traitor_y - 8)
-			end
-			return
+		local bar_height = 25
+		local bar_width = width - (margin*2)
+		local health = math.max(0, ply:Health())
+		local health_y = y + margin
+
+		PaintBar(x + margin, health_y, bar_width, bar_height, health_colors, health/100)
+		ShadowedText(tostring(health), "HealthAmmo", bar_width, health_y, COLOR_WHITE, TEXT_ALIGN_RIGHT, TEXT_ALIGN_RIGHT)
+
+		if ttt_health_label:GetBool() then
+			local health_status = util.HealthToString(health)
+			draw.SimpleText(L[health_status], "TabLarge", x + margin*2, health_y + bar_height/2, COLOR_WHITE, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
 		end
-		return old_DrawHUD(self)
+
+		if ply:GetActiveWeapon().Primary then
+			local ammo_clip, ammo_max, ammo_inv = GetAmmo(ply)
+			if ammo_clip != -1 then
+				local ammo_y = health_y + bar_height + margin
+				PaintBar(x+margin, ammo_y, bar_width, bar_height, ammo_colors, ammo_clip/ammo_max)
+
+				local text = string.format("%i + %02i", ammo_clip, ammo_inv)
+				ShadowedText(text, "HealthAmmo", bar_width, ammo_y, COLOR_WHITE, TEXT_ALIGN_RIGHT, TEXT_ALIGN_RIGHT)
+			end
+		end
+
+		local text = "Ghost DM"
+		local traitor_y = y - 30
+		ShadowedText(text, "TraitorState", x + margin + 73, traitor_y, COLOR_WHITE, TEXT_ALIGN_CENTER)
+
+		local is_haste = HasteMode() and GetRoundState() == ROUND_ACTIVE
+		local color = COLOR_WHITE
+		local rx = x + margin + 170
+		local ry = traitor_y + 3
+
+		local blab = util.SimpleTime(math.max(0, GetGlobalFloat("ttt_round_end", 0) - CurTime()), "%02i:%02i")
+		ShadowedText(blab, "TimeLeft", rx, ry, color)
+
+		if is_haste then
+			dr.SimpleText(L.hastemode, "TabLarge", x + margin + 165, traitor_y - 8)
+		end
 	end
-		
+
+	WSWITCH:Draw(ply)
+	VOICE.Draw(ply)
+	hook.Call("HUDDrawPickupHistory", GAMEMODE)
+end)
+
+hook.Add("HUDShouldDraw", "GhostHUD", function(n)
+	local p = LocalPlayer()
+	if p:IsPlayer() and p:IsGhost() and n == "TTTSpecHUD" then return false end
 end)
